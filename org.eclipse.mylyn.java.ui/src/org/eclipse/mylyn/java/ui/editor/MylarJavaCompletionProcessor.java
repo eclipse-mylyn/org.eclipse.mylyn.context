937a1d897664463645770f4a30a6050259cee9fe
mkersten <>
2005-12-13 16:31:49 +0000

    3.1 merge

--- org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/editor/MylarJavaCompletionProcessor.java
+++ org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/editor/MylarJavaCompletionProcessor.java
@@ -17,6 +17,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.internal.ui.text.java.JavaCompletionProcessor;
@@ -31,6 +32,7 @@ import org.eclipse.mylar.core.MylarPlugin;
 import org.eclipse.mylar.core.internal.MylarContextManager;
 import org.eclipse.mylar.core.internal.dt.MylarWebRef;
 import org.eclipse.mylar.core.util.ErrorLogger;
+import org.eclipse.mylar.ui.MylarImages;
 import org.eclipse.ui.IEditorPart;
 
 /**
@@ -40,23 +42,33 @@ import org.eclipse.ui.IEditorPart;
  */
 public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
 
+    private static final String LABEL_SEPARATOR = " -----------------------------------";
+
+    private Method resolveMemberMethod;
+    
     public MylarJavaCompletionProcessor(IEditorPart editor) {
         super(editor); 
+        try {
+			resolveMemberMethod = MemberProposalInfo.class.getDeclaredMethod("resolveMember", new Class[] { } );
+	        resolveMemberMethod.setAccessible(true);
+        } catch (SecurityException e) {
+			ErrorLogger.fail(e, "could not install content assist, reflection denied", false);
+		} catch (NoSuchMethodException e) {
+	    	ErrorLogger.fail(e, "could not install content assist, wrong Eclipse version", false);
+		}
     }
     
     @MylarWebRef(name="Reflection documentation", url="http://www.onjava.com/pub/a/onjava/2003/11/12/reflection.html?page=last")
     @Override
     public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {    
-        try {
-            Method method = MemberProposalInfo.class.getDeclaredMethod("resolveMember", new Class[] { } );
-            method.setAccessible(true);
-            ICompletionProposal[] proposals = super.computeCompletionProposals(viewer, offset);
-    
-            TreeMap<Float, ICompletionProposal> interesting = new TreeMap<Float, ICompletionProposal>();
+        ICompletionProposal[] proposals = super.computeCompletionProposals(viewer, offset);
+        if (resolveMemberMethod == null || !MylarPlugin.getContextManager().hasActiveContext()) return proposals;
+        try {            
+        	TreeMap<Float, ICompletionProposal> interesting = new TreeMap<Float, ICompletionProposal>();
             List<ICompletionProposal> rest = new ArrayList<ICompletionProposal>();
             int unresolvedProposals = 0;
-            for (int i = 0; i < proposals.length; i++) {
-                ICompletionProposal proposal = proposals[i];
+            for (Object proposalObject : proposals) {
+            	ICompletionProposal proposal = (ICompletionProposal)proposalObject;
                 ProposalInfo info = null;
                 if (proposal instanceof JavaCompletionProposal) {
                     info = ((JavaCompletionProposal)proposal).getProposalInfo();
@@ -64,12 +76,11 @@ public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
                     info = ((LazyJavaCompletionProposal)proposal).getProposalInfo();
                 } 
                 boolean added = false;
-                try { // HACK
+                try {
                     if (info != null) {
-                        IMember member = null; 
-                        if (info instanceof MemberProposalInfo) member = (IMember)method.invoke(info, new Object[] { });
+                        IMember member = null; // HACK: using reflection to get member
+                        if (info instanceof MemberProposalInfo) member = (IMember)resolveMemberMethod.invoke(info, new Object[] { });
                         if (member == null || MylarPlugin.getContextManager().getActiveContext() == null) {
-                            // nothing for now
                         	rest.add(proposal);
                         } else {
                         	IMylarElement node = MylarPlugin.getContextManager().getElement(member.getHandleIdentifier()); 
@@ -80,6 +91,8 @@ public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
 	                            } else {
 	                            	rest.add(proposal);
 	                            }
+                            } else {
+                            	rest.add(proposal);
                             }
                         } 
                         added = true;
@@ -101,10 +114,10 @@ public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
             } else {
                 ICompletionProposal[] sorted = new ICompletionProposal[interesting.keySet().size() + rest.size() + 1];
                 int i = 0;
-                for (Float f : interesting.keySet()) {
-                    sorted[i] = interesting.get(f);
+                for (Entry<Float, ICompletionProposal> entry : interesting.entrySet()) {
+                    sorted[i] = entry.getValue();
                     i++; 
-                }    
+                } 
                 if (interesting.keySet().size() > 0) {
                     int replacementOffset = -1;
                     if (sorted[i-1] instanceof JavaCompletionProposal) {
@@ -112,9 +125,12 @@ public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
                     } else if (sorted[i-1] instanceof LazyJavaCompletionProposal) {
                         replacementOffset = ((LazyJavaCompletionProposal)sorted[i-1]).getReplacementOffset();
                     } else {
-                    	ErrorLogger.log("Could not create proposal separator for class: " + sorted[i-1].getClass(), this);
+                        ErrorLogger.log("Could not create proposal separator for class: " + sorted[i-1].getClass(), this);
                     }
-                    sorted[i] = new JavaCompletionProposal("", replacementOffset, 0, null, "----------------", 0);
+                    sorted[i] = new JavaCompletionProposal(
+                    		"", replacementOffset, 0, 
+                    		MylarImages.getImage(MylarImages.CONTENT_ASSIST_SEPARATOR), 
+                    		LABEL_SEPARATOR, 0);
                     i++;
                 } 
                 for (ICompletionProposal proposal : rest) {
@@ -123,8 +139,8 @@ public class MylarJavaCompletionProcessor extends JavaCompletionProcessor {
                 }
                 return sorted;
             }
-        } catch (Exception e) {
-        	ErrorLogger.log(e, "completion proposal failed");
+        } catch (Throwable t) {
+        	ErrorLogger.log(t, "completion proposal filter and sort failed");
         }
         return null;
     } 
