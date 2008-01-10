b321c47aec60cb11dc7e997d15d8020124deee5e
spingel <>
2008-01-10 02:47:42 +0000

    RESOLVED - bug 208819: "Report as Bug" action in Error Log view should be available only for log entries https://bugs.eclipse.org/bugs/show_bug.cgi?id=208819

--- org.eclipse.mylyn.bugzilla.ide/src/org/eclipse/mylyn/internal/bugzilla/ide/actions/NewTaskFromErrorAction.java
+++ org.eclipse.mylyn.bugzilla.ide/src/org/eclipse/mylyn/internal/bugzilla/ide/actions/NewTaskFromErrorAction.java
@@ -9,32 +9,23 @@
 package org.eclipse.mylyn.internal.bugzilla.ide.actions;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.mylyn.internal.tasks.ui.ITasksUiConstants;
-import org.eclipse.mylyn.internal.tasks.ui.wizards.NewTaskWizard;
-import org.eclipse.mylyn.tasks.ui.editors.AbstractRepositoryTaskEditor;
-import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.mylyn.tasks.core.TaskSelection;
+import org.eclipse.mylyn.tasks.ui.TasksUiUtil;
 import org.eclipse.pde.internal.runtime.logview.LogEntry;
-import org.eclipse.swt.dnd.Clipboard;
-import org.eclipse.swt.dnd.TextTransfer;
-import org.eclipse.swt.dnd.Transfer;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * Creates a new task from the selected error log entry.
  * 
  * @author Jeff Pound
+ * @author Steffen Pingel
  */
 public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionChangedListener {
 
@@ -44,54 +35,35 @@ public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionCh
 
 	public void run() {
 		TreeItem[] items = treeViewer.getTree().getSelection();
-		LogEntry selection = null;
+		LogEntry entry = null;
 		if (items.length > 0) {
-			selection = (LogEntry) items[0].getData();
+			entry = (LogEntry) items[0].getData();
 		}
-		if (selection == null) {
+		if (entry == null) {
 			return;
 		}
-		NewTaskWizard wizard = new NewTaskWizard();
+		
+		createTask(entry);
+	}
 
-		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		if (shell != null && !shell.isDisposed()) {
-			WizardDialog dialog = new WizardDialog(shell, wizard);
-			dialog.setBlockOnOpen(true);
-			if (dialog.open() == WizardDialog.CANCEL) {
-				return;
-			}
-
-			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			AbstractRepositoryTaskEditor editor = null;
-
-			String summary = "";
-//				selection.getSeverityText() + ": \"" + selection.getMessage() + "\" in "
-//					+ selection.getPluginId();
-			String description = "\n\n-- Error Log --\nDate: " + selection.getDate() + "\nMessage: "
-					+ selection.getMessage() + "\nSeverity: " + selection.getSeverityText() + "\nPlugin ID: "
-					+ selection.getPluginId() + "\nStack Trace:\n"
-					+ ((selection.getStack() == null) ? "no stack trace available" : selection.getStack());
-
-			try {
-				TaskEditor taskEditor = (TaskEditor) page.getActiveEditor();
-				editor = (AbstractRepositoryTaskEditor) taskEditor.getActivePageInstance();
-			} catch (ClassCastException e) {
-				Clipboard clipboard = new Clipboard(page.getWorkbenchWindow().getShell().getDisplay());
-				clipboard.setContents(new Object[] { summary + "\n" + description },
-						new Transfer[] { TextTransfer.getInstance() });
-
-				MessageDialog.openInformation(
-						page.getWorkbenchWindow().getShell(),
-						ITasksUiConstants.TITLE_DIALOG,
-						"This connector does not provide a rich task editor for creating tasks.\n\n"
-								+ "The error contents have been placed in the clipboard so that you can paste them into the entry form.");
-				return;
-			}
-
-			editor.setSummaryText(summary);
-			editor.setDescriptionText(description);
-
+	protected void createTask(LogEntry entry) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n\n-- Error Log --\nDate: ");
+		sb.append(entry.getDate());
+		sb.append("\nMessage: ");
+		sb.append(entry.getMessage());
+		sb.append("\nSeverity: " + entry.getSeverityText());
+		sb.append("\nPlugin ID: ");
+		sb.append(entry.getPluginId());
+		sb.append("\nStack Trace:\n");
+		if (entry.getStack() == null) {
+			sb.append("no stack trace available");
+		} else {
+			sb.append(entry.getStack());
 		}
+
+		TaskSelection taskSelection = new TaskSelection("", sb.toString());
+		TasksUiUtil.openNewTaskWizard(taskSelection, false);
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
