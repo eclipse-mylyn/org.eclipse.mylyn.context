0343d738c369be5b545a5531e1fcfe92620e67a0
spingel <>
2008-01-31 23:59:05 +0000

    RESOLVED - bug 215071: [patch] report as Bug should include subentries too https://bugs.eclipse.org/bugs/show_bug.cgi?id=215071

--- org.eclipse.mylyn.bugzilla.ide/src/org/eclipse/mylyn/internal/bugzilla/ide/actions/NewTaskFromErrorAction.java
+++ org.eclipse.mylyn.bugzilla.ide/src/org/eclipse/mylyn/internal/bugzilla/ide/actions/NewTaskFromErrorAction.java
@@ -9,6 +9,7 @@
 package org.eclipse.mylyn.internal.bugzilla.ide.actions;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -35,21 +36,17 @@ public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionCh
 
 	private TreeViewer treeViewer;
 
-	public void run() {
-		TreeItem[] items = treeViewer.getTree().getSelection();
-		LogEntry entry = null;
-		if (items.length > 0) {
-			entry = (LogEntry) items[0].getData();
-		}
-		if (entry == null) {
-			return;
-		}
-		
-		createTask(entry);
-	}
-
-	protected void createTask(LogEntry entry) {
-		StringBuilder sb = new StringBuilder();
+	/**
+	 * Fills a {@link StringBuilder} with {@link LogEntry} information, optionally including subentries too
+	 * 
+	 * @param entry
+	 *            The {@link LogEntry} who provides the information
+	 * @param sb
+	 *            An {@link StringBuilder} to be filled with
+	 * @param includeChildren
+	 *            Indicates if it should include subentries, if the {@link LogEntry} have any
+	 */
+	private void buildDescriptionFromLogEntry(LogEntry entry, StringBuilder sb, boolean includeChildren) {
 		sb.append("\n\n-- Error Log --\nDate: ");
 		sb.append(entry.getDate());
 		sb.append("\nMessage: ");
@@ -64,14 +61,29 @@ public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionCh
 			sb.append(entry.getStack());
 		}
 
-		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		TaskSelection taskSelection = new TaskSelection("", sb.toString());
-		TasksUiUtil.openNewTaskEditor(shell, taskSelection, null);
+		if (includeChildren && entry.hasChildren()) {
+			Object[] children = entry.getChildren(null);
+			for (Object child : children) {
+				if (child instanceof LogEntry) {
+					buildDescriptionFromLogEntry((LogEntry) child, sb, includeChildren);
+				}
+			}
+		}
 	}
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		// this selection is always empty? explicitly register a listener in
-		// init() instead
+	protected void createTask(LogEntry entry) {
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		boolean includeChildren = false;
+
+		if (entry.hasChildren() && MessageDialog.openQuestion(shell, "Report Bug", "Include children of this entry in the report?")) {
+			includeChildren = true;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		buildDescriptionFromLogEntry(entry, sb, includeChildren);
+
+		TaskSelection taskSelection = new TaskSelection("", sb.toString());
+		TasksUiUtil.openNewTaskEditor(shell, taskSelection, null);
 	}
 
 	public void init(IViewPart view) {
@@ -80,11 +92,30 @@ public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionCh
 		sp.setSelection(sp.getSelection());
 	}
 
+	public void run() {
+		TreeItem[] items = treeViewer.getTree().getSelection();
+		LogEntry entry = null;
+		if (items.length > 0) {
+			entry = (LogEntry) items[0].getData();
+		}
+		if (entry == null) {
+			return;
+		}
+		
+		createTask(entry);
+	}
+
 	public void run(IAction action) {
 		run();
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		// this selection is always empty? explicitly register a listener in
+		// init() instead
 	}
 
 	public void selectionChanged(SelectionChangedEvent event) {
 		treeViewer = (TreeViewer) event.getSource();
 	}
+	
 }
